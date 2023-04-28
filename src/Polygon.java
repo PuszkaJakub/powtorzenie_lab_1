16d1e0378a5fc52abf97801e03c6817fd6679b63
puszk <puszka.kontakt@gmail.com>
2023-04-28 20:28:37 +0200

    lab2 zad3

--- src/Polygon.java
+++ src/Polygon.java
@@ -2,6 +2,12 @@ public class Polygon {
     private Point[] arr;
     public Style style;
 
+
+    public Polygon(Point[] arr, Style style) {
+        this.arr = arr;
+        this.style = style;
+    }
+
     public Polygon(int vertices, Style style) {
         this.arr = new Point[vertices];
         this.style = style;
@@ -32,5 +38,19 @@ public class Polygon {
 
         result += "/>\n</svg>\n";
         return result;
+    }
+
+
+    public static Polygon square(Segment line, Style style){
+//        Point a = new Point(line.getP1().x, line.getP1().y);
+//        Point b = new Point(line.getP1().x, line.getP2().y);
+//        Point c = new Point(line.getP2().x, line.getP2().y);
+//        Point d = new Point(line.getP2().x, line.getP1().y);
+
+        Point[] tempArr = {new Point(line.getP1().x, line.getP1().y),   //a
+                new Point(line.getP1().x, line.getP2().y),  //b
+                new Point(line.getP2().x, line.getP2().y),  //c
+                new Point(line.getP2().x, line.getP1().y)}; //d
+        return new Polygon(tempArr, style);
     }
 }
